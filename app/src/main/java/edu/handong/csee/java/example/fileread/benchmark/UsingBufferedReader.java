fe1169d22b5963178dce84b1b2ac1b34b750ac47
KKIMYENN <145086605+KKIMYENN@users.noreply.github.com>
2023-11-17 21:43:06 +0900

    modified code to print the file name out.

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -7,10 +7,9 @@ import java.io.*;
  */
 public class UsingBufferedReader {
     public static void main(String[] args) throws IOException {
-   
+    System.out.println(args[0]);
 
-
-        //-------------- Test reading 1 MB file. --------------------
+        //-------------- Test reading  file. --------------------
 
         StopWatch.start();
 
@@ -23,17 +22,17 @@ public class UsingBufferedReader {
         inputStream.close();
 
 
-        //-------------- Test reading 10 MB file. --------------------
-
-        StopWatch.start();
-
-        BufferedReader inputStream2= new BufferedReader(new FileReader(args[0]));
-        while (inputStream2.read()!=-1){}
-
-        long duration2 = StopWatch.stop();
-        System.out.println(duration2 + " milsec");
-
-        inputStream2.close();
+//        //-------------- Test reading 10 MB file. --------------------
+//
+//        StopWatch.start();
+//
+//        BufferedReader inputStream2= new BufferedReader(new FileReader(args[0]));
+//        while (inputStream2.read()!=-1){}
+//
+//        long duration2 = StopWatch.stop();
+//        System.out.println(duration2 + " milsec");
+//
+//        inputStream2.close();
 
         /*
         //-------------- Test reading 100 MB file. --------------------
